4eb2d981313d5f8fc159ea63b85ed4620072e6bb
motty93 <rdwbocungelt5@gmail.com>
2022-07-28 13:58:06 +0900

    feat: user 処理

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common'
+import { Serialize } from 'src/interceptors/serialize.interceptor'
+import { CreateUserDto } from './dtos/create-user.dto'
+import { UpdateUserDto } from './dtos/update-user.dto'
+import { UserDto } from './dtos/user.dto'
+import { UsersService } from './users.service'
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('/sign_up')
+  async createUser(@Body() dto: CreateUserDto) {
+    return await this.usersService.create(dto.email, dto.password)
+  }
+
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
+  @Serialize(UserDto)
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    console.log('handler is running')
+    const user = await this.usersService.findOne(parseInt(id))
+    if (!user) {
+      throw new NotFoundException('user not found')
+    }
+
+    return user
+  }
+
+  @Get()
+  async findAllUsers(@Query('email') email: string) {
+    return await this.usersService.find(email)
+  }
+
+  @Delete('/:id')
+  async removeUser(@Param('id') id: string) {
+    return await this.usersService.remove(parseInt(id))
+  }
+
+  @Patch('/:id')
+  async updateUser(@Param('id') id: string, @Body() dto: UpdateUserDto) {
+    return await this.usersService.update(parseInt(id), dto)
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { User } from './users.entities'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly repo: Repository<User>,
+  ) {}
+
+  async create(email: string, password: string) {
+    const user = this.repo.create({ email, password })
+
+    return await this.repo.save(user)
+  }
+
+  async findOne(id: number) {
+    return await this.repo.findOne({ where: { id } })
+  }
+
+  async find(email: string) {
+    return await this.repo.find({ where: { email } })
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id)
+    if (!user) {
+      throw new Error('user not found')
+    }
+    Object.assign(user, attrs)
+
+    return await this.repo.save(user)
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id)
+    if (!user) {
+      throw new Error('user not found')
+    }
+
+    return await this.repo.remove(user)
+  }
+}
