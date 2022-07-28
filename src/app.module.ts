4e684de8f3dd29448c618ffab56c2828a468ad65
motty93 <rdwbocungelt5@gmail.com>
2022-07-28 13:57:26 +0900

    feat: typeorm setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
 import { ReportsModule } from './reports/reports.module'
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [TypeOrmModule.forRoot(), UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
